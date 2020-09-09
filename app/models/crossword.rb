80e577d89c06d786e48267923d9f07efbe12fbf1
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-09 16:20:07 -0500

    add methods to get difficulty levels

--- app/models/crossword.rb
+++ app/models/crossword.rb
@@ -1,2 +1,22 @@
 class Crossword < ApplicationRecord
+
+    def makeDate 
+        Date.strptime(self.date, '%m/%d/%Y')
+    end
+
+    def self.easy
+        self.select{|crswd| crswd.makeDate.monday? || crswd.makeDate.tuesday?}
+    end
+
+    def self.medium
+        self.select{|crswd| crswd.makeDate.wednesday? || crswd.makeDate.thursday?}
+    end
+
+    def self.hard
+        self.select{|crswd| crswd.makeDate.friday? || crswd.makeDate.saturday?}
+    end
+
+    def self.sunday
+        self.select{|crswd| crswd.makeDate.sunday?}
+    end
 end
