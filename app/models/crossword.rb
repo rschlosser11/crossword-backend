48fab3dd2b2c93fccde37a325e401a2365a96089
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-09 20:08:55 -0500

    normalize title after validation

--- app/models/crossword.rb
+++ app/models/crossword.rb
@@ -1,22 +1,37 @@
 class Crossword < ApplicationRecord
+    after_validation :titlecase_title, on: [:create, :update]
 
-    def makeDate 
+    def make_date 
         Date.strptime(self.date, '%m/%d/%Y')
     end
 
     def self.easy
-        self.select{|crswd| crswd.makeDate.monday? || crswd.makeDate.tuesday?}
+        self.select{|crswd| crswd.make_date.monday? || crswd.make_date.tuesday?}
     end
 
     def self.medium
-        self.select{|crswd| crswd.makeDate.wednesday? || crswd.makeDate.thursday?}
+        self.select{|crswd| crswd.make_date.wednesday? || crswd.make_date.thursday?}
     end
 
     def self.hard
-        self.select{|crswd| crswd.makeDate.friday? || crswd.makeDate.saturday?}
+        self.select{|crswd| crswd.make_date.friday? || crswd.make_date.saturday?}
     end
 
     def self.sunday
-        self.select{|crswd| crswd.makeDate.sunday?}
+        self.select{|crswd| crswd.make_date.sunday?}
+    end
+
+    private
+
+    def titlecase_title
+        title_arr = self.title.split(' ')
+        title_arr.map! do |word|
+            if word == "NY"
+                word
+            else
+                word.titleize;
+            end
+        end
+        self.title = title_arr.join(' ')
     end
 end
