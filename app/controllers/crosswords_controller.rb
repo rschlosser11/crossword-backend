b9953c552d45b90fee2f1506baba5c068e998884
rschlosser11 <rosalinschlosser@gmail.com>
2020-09-14 16:12:54 -0500

    make sure params[:id] is an integer

--- app/controllers/crosswords_controller.rb
+++ app/controllers/crosswords_controller.rb
@@ -1,12 +1,24 @@
 class CrosswordsController < ApplicationController 
     def index
-        crosswords = Crossword.all 
+        if params[:q]
+            if params[:q] == 'easy'
+                crosswords = Crossword.easy
+            elsif params[:q] == 'medium'
+                crosswords = Crossword.medium
+            elsif params[:q] == 'hard'
+                crosswords = Crossword.hard
+            elsif params[:q] == 'sunday'
+                crosswords = Crossword.sunday
+            end
+        else
+            crosswords = Crossword.all 
+        end
 
         render json: crosswords, except: [:created_at, :updated_at]
     end
 
     def show
-        crossword = Crossword.find(params[:id])
+        crossword = Crossword.find(params[:id].to_i)
 
         render json: crossword, except: [:created_at, :updated_at]
     end
